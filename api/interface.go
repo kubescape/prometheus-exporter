e92de2401c332cf01deecec04f9334fe9abf6918
Matthias Bertschy <matthias.bertschy@gmail.com>
2023-12-28 23:31:43 +0100

    use in-cluster config for client when possible
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

--- api/interface.go
+++ api/interface.go
@@ -3,7 +3,6 @@ package api
 import "github.com/kubescape/storage/pkg/apis/softwarecomposition/v1beta1"
 
 type StorageClient interface {
-	GetVulnerabilitySummaries(kubeconfig string) (*v1beta1.VulnerabilitySummaryList, error)
-	GetConfigScanSummaries(kubeconfig string) (*v1beta1.ConfigurationScanSummaryList, error)
-	Initialize(kubeconfig string) error
+	GetVulnerabilitySummaries() (*v1beta1.VulnerabilitySummaryList, error)
+	GetConfigScanSummaries() (*v1beta1.ConfigurationScanSummaryList, error)
 }
--- main.go
+++ main.go
@@ -1,35 +1,21 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"path/filepath"
 
 	"github.com/kubescape/go-logger"
 	"github.com/kubescape/go-logger/helpers"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/kubescape/prometheus-exporter/api"
 	"github.com/kubescape/prometheus-exporter/metrics"
-	"k8s.io/client-go/util/homedir"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-
 	var storageClient api.StorageClient
 	storageClient = api.NewStorageClient()
-	if err := storageClient.Initialize(*kubeconfig); err != nil {
-		logger.L().Error("error initializing storage client", helpers.Error(err))
-	}
 
 	// Start Prometheus HTTP server
 	go func() {
@@ -41,12 +27,12 @@ func main() {
 	// monitor the severities in objects
 	for {
 
-		configScanSummaries, err := storageClient.GetConfigScanSummaries(*kubeconfig)
+		configScanSummaries, err := storageClient.GetConfigScanSummaries()
 		if err != nil {
 			logger.L().Error("error getting configuration scan summaries", helpers.Error(err))
 		}
 
-		vulnScanSummaries, err := storageClient.GetVulnerabilitySummaries(*kubeconfig)
+		vulnScanSummaries, err := storageClient.GetVulnerabilitySummaries()
 		if err != nil {
 			logger.L().Error("error getting vulnerability scan summaries", helpers.Error(err))
 		}
@@ -57,5 +43,5 @@ func main() {
 		metrics.ProcessConfigscanClusterMetrics(configScanSummaries)
 		metrics.ProcessConfigscanNamespaceMetrics(configScanSummaries)
 	}
-	
+
 }
